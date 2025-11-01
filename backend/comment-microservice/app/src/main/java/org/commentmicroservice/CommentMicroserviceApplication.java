debcc3997a42834fac68bf8eaefd1e254aaf4126
hoocken <alexander.anthony.tang@gmail.com>
2025-11-01 15:30:59 +0100

    Changed backend to java and renamed python folder to ml

---
+++ backend/comment-microservice/app/src/main/java/org/commentmicroservice/CommentMicroserviceApplication.java
@@ -0,0 +1,11 @@
+package org.commentmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentMicroserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommentMicroserviceApplication.class, args);
+    }
+}
---
+++ backend/comment-microservice/app/src/main/java/org/commentmicroservice/model/Databasehelper.java
@@ -0,0 +1,8 @@
+package main.java.org.commentmicroservice.model;
+
+import java.sql.*;
+
+public class Databasehelper {
+    
+}
+
